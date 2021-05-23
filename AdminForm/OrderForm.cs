d1efa1ec4df85d8be2d91445f37719cefba646fb
nanopeniss <trongga9a@gmail.com>
2021-05-23 20:58:31 +0700

    update Order Form

--- AdminForm/OrderForm.cs
+++ AdminForm/OrderForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UC_Mon;
+using DTO;
+using BLL;
 
 namespace AdminForm
 {
@@ -15,6 +18,36 @@ namespace AdminForm
         public OrderForm()
         {
             InitializeComponent();
+        }
+
+        private void OrderForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnDoAn_Click(object sender, EventArgs e)
+        {
+            pnMons.Controls.Clear();
+            List<Mon> listMon = BusinessLogicLayer.Instance.GetAllMon();
+            for (int i = 0; i < listMon.Count; i++)
+            {
+                UCMon mon = new UCMon();
+                mon.GiaTien = listMon[i].GiaTien;
+                mon.TenMon = listMon[i].TenMon;
+
+
+                Image anh = Image.FromStream(BusinessLogicLayer.Instance.GetByteValuesOfAnh(listMon[i].IdAnh));
+                mon.AnhMinhHoa = anh;
+                mon.Tag = listMon[i];
+
+                mon.Click += new EventHandler(btnMon_Click);
+                pnMons.Controls.Add(mon);
+
+            }
+        }
+
+        private void btnMon_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("đâsdasdasda");
         }
     }
 }
