c48fc7b966ba078c1015ddd9ecf47248cc5179ad
hobathanh <hobathanh201@.com>
2021-06-24 22:49:00 +0700

    Update From admin

--- AdminForm/LoginForm.Designer.cs
+++ AdminForm/LoginForm.Designer.cs
@@ -43,10 +43,10 @@ namespace AdminForm
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(107, 66);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(82, 70);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(185, 179);
+            this.pictureBox1.Size = new System.Drawing.Size(139, 145);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -57,9 +57,10 @@ namespace AdminForm
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(56, 329);
+            this.label1.Location = new System.Drawing.Point(42, 267);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 27);
+            this.label1.Size = new System.Drawing.Size(83, 22);
             this.label1.TabIndex = 3;
             this.label1.Text = "Manager";
             // 
@@ -69,9 +70,10 @@ namespace AdminForm
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(232, 329);
+            this.label2.Location = new System.Drawing.Point(174, 267);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 27);
+            this.label2.Size = new System.Drawing.Size(93, 22);
             this.label2.TabIndex = 4;
             this.label2.Text = "Customer";
             // 
@@ -80,10 +82,10 @@ namespace AdminForm
             this.ptEixt.BackColor = System.Drawing.Color.Red;
             this.ptEixt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ptEixt.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ptEixt.Location = new System.Drawing.Point(334, -1);
-            this.ptEixt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ptEixt.Location = new System.Drawing.Point(250, -1);
+            this.ptEixt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ptEixt.Name = "ptEixt";
-            this.ptEixt.Size = new System.Drawing.Size(67, 32);
+            this.ptEixt.Size = new System.Drawing.Size(50, 26);
             this.ptEixt.TabIndex = 5;
             this.ptEixt.Text = "X";
             this.ptEixt.UseVisualStyleBackColor = false;
@@ -103,14 +105,15 @@ namespace AdminForm
             this.btnManager.Image = ((System.Drawing.Image)(resources.GetObject("btnManager.Image")));
             this.btnManager.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.btnManager.ImageSize = new System.Drawing.Size(105, 105);
-            this.btnManager.Location = new System.Drawing.Point(35, 357);
+            this.btnManager.Location = new System.Drawing.Point(26, 290);
+            this.btnManager.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnManager.Name = "btnManager";
             this.btnManager.OnHoverBaseColor = System.Drawing.Color.Transparent;
             this.btnManager.OnHoverBorderColor = System.Drawing.Color.Black;
             this.btnManager.OnHoverForeColor = System.Drawing.Color.White;
             this.btnManager.OnHoverImage = ((System.Drawing.Image)(resources.GetObject("btnManager.OnHoverImage")));
             this.btnManager.OnPressedColor = System.Drawing.Color.Black;
-            this.btnManager.Size = new System.Drawing.Size(147, 143);
+            this.btnManager.Size = new System.Drawing.Size(110, 116);
             this.btnManager.TabIndex = 7;
             this.btnManager.Click += new System.EventHandler(this.ptadmin_Click);
             // 
@@ -128,25 +131,26 @@ namespace AdminForm
             this.btnCustomer.Image = ((System.Drawing.Image)(resources.GetObject("btnCustomer.Image")));
             this.btnCustomer.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.btnCustomer.ImageSize = new System.Drawing.Size(105, 105);
-            this.btnCustomer.Location = new System.Drawing.Point(214, 357);
+            this.btnCustomer.Location = new System.Drawing.Point(160, 290);
+            this.btnCustomer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCustomer.Name = "btnCustomer";
             this.btnCustomer.OnHoverBaseColor = System.Drawing.Color.Transparent;
             this.btnCustomer.OnHoverBorderColor = System.Drawing.Color.Black;
             this.btnCustomer.OnHoverForeColor = System.Drawing.Color.White;
             this.btnCustomer.OnHoverImage = ((System.Drawing.Image)(resources.GetObject("btnCustomer.OnHoverImage")));
             this.btnCustomer.OnPressedColor = System.Drawing.Color.Black;
-            this.btnCustomer.Size = new System.Drawing.Size(147, 143);
+            this.btnCustomer.Size = new System.Drawing.Size(110, 116);
             this.btnCustomer.TabIndex = 7;
             this.btnCustomer.Click += new System.EventHandler(this.ptcustomer_Click);
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(400, 512);
+            this.ClientSize = new System.Drawing.Size(300, 416);
             this.Controls.Add(this.btnCustomer);
             this.Controls.Add(this.btnManager);
             this.Controls.Add(this.ptEixt);
@@ -155,7 +159,7 @@ namespace AdminForm
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LoginForm";
